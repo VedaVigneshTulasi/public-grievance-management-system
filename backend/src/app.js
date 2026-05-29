0a9487dd87db0840d0116d45137aade84d446c20
Tulasi Veda Vignesh <tulasi.vignesh@criticalriver.com>
2026-05-29 12:50:59 +0530

    Initialized backend setup

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Public Grievance API Running",
+  });
+});
+
+export default app;
